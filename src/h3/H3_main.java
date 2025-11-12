bae64ede494210573dcdeb1a8dd6afa47738c0f4
Justus0008 <76709522+Justus0008@users.noreply.github.com>
2025-11-12 14:52:43 +0100

    added H3

---
+++ src/h3/H3_main.java
@@ -0,0 +1,11 @@
+package h3;
+
+public class H3_main {
+	public static void main(String[] args ){
+		float celsiusFloat = 14.2f;
+		double celsiusDouble = 14.2;
+		float fahrenheitFloat = (celsiusFloat) * 1.8f + 32f;
+		double fahrenheitDouble = (celsiusDouble) * 1.8 +32.0;
+	}
+
+}
